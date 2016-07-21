1d3b97925ce9e903af631002c590ddb8dab31eb3
Bobby <sgmaniac1255@yahoo.com>
2016-07-20 20:54:36 -0500

    fixed errors in StandardDeck.java resulting from the new enum types

--- src/decks/StandardDeck.java
+++ src/decks/StandardDeck.java
@@ -2,8 +2,6 @@ package decks;
 
 import java.util.ArrayList;
 
-import javax.activity.InvalidActivityException;
-
 /**
  * @author Bobby
  *
@@ -13,23 +11,15 @@ public class StandardDeck {
 	private ArrayList<PlayingCard> deck;
 
 	/**
-	 * @param sort determines if the deck is sorted on creation or shuffled. input a 0 for sorted 1 for shuffled
+	 * Generates a new Sorted Deck of Standard Playing Cards.
 	 */
-	public StandardDeck(int sort) throws IndexOutOfBoundsException{
+	public StandardDeck(){
 		super();
 		deck = new ArrayList<PlayingCard>();
-		for (int i=1;i<=4;i++){
-			for(int j = 1;j<=13;j++){
-				deck.add(new PlayingCard(i,j));
+		for (SuitType suit : SuitType.values()) {
+			for (RankType rank : RankType.values()) {
+				deck.add(new PlayingCard(suit, rank));				
 			}
-		}
-	}
-	
-	public Card getCard(int index){	
-		return deck.get(index);
-	}
-	
-	public Card draw(){
-		return deck.remove(0);
+		} 
 	}
 }
