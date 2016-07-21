b1b1f85dfc30699fea6ab1abf90d54bbeb01ea9d
Bobby <sgmaniac1255@yahoo.com>
2016-07-20 20:33:44 -0500

    Fixed errors in PlayingCard.java that resulted from the new enum classes

--- src/decks/PlayingCard.java
+++ src/decks/PlayingCard.java
@@ -4,65 +4,25 @@ package decks;
  * @author Bobby
  *
  */
-public class PlayingCard extends Card {
+public class PlayingCard {
 
-	static public final int ACE = 1;
-	static public final int JACK = 11;
-	static public final int QUEEN = 12;
-	static public final int KING = 13;
-	static public final int SPADES = 1;
-	static public final int DIAMONDS = 2;
-	static public final int CLUBS = 3;
-	static public final int HEARTS = 4;	
-	private int suit;
+	private final SuitType SUIT;
+	private final RankType RANK;
 	
 	
-	public PlayingCard(int suit, int value) {
+	public PlayingCard(SuitType suit, RankType rank) {
 		super();
-		this.suit = suit;
-		this.value = value;
+		this.SUIT = suit;
+		this.RANK = rank;
 	}
-	
-	public int getSuit() {
-		return suit;
+		
+	public SuitType getSuit() {
+		return SUIT;
 	}
 	
 	@Override
 	public String toString() {
-		String rtn = "";
-		switch (value){
-		case ACE:
-			rtn += "Ace";
-			break;
-		case JACK:
-			rtn += "Jack";
-			break;
-		case QUEEN:
-			rtn += "Queen";
-			break;
-		case KING:
-			rtn += "King";
-			break;
-		default:
-			rtn += value;
-		}
-		rtn += " of ";
-		switch (suit){
-		case SPADES:
-			rtn += "Spades!";
-			break;
-		case DIAMONDS:
-			rtn += "Diamonds!";
-			break;
-		case CLUBS:
-			rtn += "Clubs!";
-			break;
-		case HEARTS:
-			rtn += "Hearts!";
-			break;
-		}
-		
-		return rtn;
+		return RANK.toString() + " of " + SUIT.toString() + "s!";
 	}
 
 }
