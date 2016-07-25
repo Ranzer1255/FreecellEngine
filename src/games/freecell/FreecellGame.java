b4f55f1ea138eda3544afd278e27740cb75851d8
Bobby <sgmaniac1255@yahoo.com>
2016-07-25 12:18:04 -0500

    adds CardPiles to represent the play area, Freecells and Foundation.
    builds the three areas in the game and their repective constructors

--- src/games/freecell/FreecellGame.java
+++ src/games/freecell/FreecellGame.java
@@ -1,11 +1,56 @@
 package games.freecell;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import decks.StandardDeck;
+import decks.Suit;
 
-public class Game {
+public class FreecellGame {
 
-	private StandardDeck deck = new StandardDeck();
+	private StandardDeck deck;
 	
+	private CardPile[] playAreaStack = new CardPile[8];
+	private CardPile[] freecell		 = new CardPile[4];
+	private Map<Suit, CardPile> foundation;
+
+	public FreecellGame(){
+		instanciatePlayAreaStack();
+		instaciateFreecell();
+		instaciateFoundation();
+	}
+
+	public void newGame(){
+	
+		//TODO
+	}
+
+	public boolean moveCard(){
+		return false; //TODO
+	}
+
+	private void instanciatePlayAreaStack() {
+		for (CardPile cardPile : playAreaStack) {
+			cardPile = new PlayFieldStack();
+		}
+	}
+
+	private void instaciateFreecell() {
+		for (CardPile cardPile : freecell) {
+			cardPile = new Freecell();
+		}
+	}
+
+	private void instaciateFoundation() {
+		foundation = new HashMap<Suit,CardPile>(4,1);
+		
+		for (Suit s : Suit.values()) {
+			foundation.put(s, new Foundation(s));
+		}
+	}
+	
+	
+
 	
 	
 }
