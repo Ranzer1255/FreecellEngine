90fd460ab18af78fc424fd8c9094ec2a42676711
Bobby <sgmaniac1255@yahoo.com>
2016-08-07 14:02:05 -0500

    reorginiziation of code

--- src/solitare/CardPile.java
+++ src/solitare/CardPile.java
@@ -9,8 +9,6 @@ public abstract class CardPile {
 
 	private Deque<PlayingCard> pile;
 
-	protected abstract boolean isLegalMove(PlayingCard newCard, PlayingCard existingCard);
-
 	public CardPile() {
 		pile = new LinkedList<PlayingCard>();
 	}
@@ -48,4 +46,6 @@ public abstract class CardPile {
 	public PlayingCard topCard(){
 		return pile.peek();
 	}
+
+	protected abstract boolean isLegalMove(PlayingCard newCard, PlayingCard existingCard);
 }
