2b97a60e7877050012e228287f63c1f3b96eec96
Bobby <sgmaniac1255@yahoo.com>
2016-08-07 14:43:25 -0500

    built draw and shuffle functions

--- src/decks/StandardDeck.java
+++ src/decks/StandardDeck.java
@@ -2,6 +2,7 @@ package decks;
 
 import java.util.ArrayList;
 import java.util.Stack;
+import java.util.concurrent.ThreadLocalRandom;
 
 /**
  * @author Bobby
@@ -29,13 +30,32 @@ public class StandardDeck {
 	 * draws the top card from the deck and moves it to the discard
 	 * @return
 	 */
-	public PlayingCard Draw(){ return null; }//TODO
+	public PlayingCard draw(){ 
+		PlayingCard c = deck.remove(0);
+		discard.push(c);
+		return c;
+	}
 	
 	/**
 	 * adds the discard back into the deck and shuffles into a random order
 	 */
-	public void shuffle(){}
-	
+	public void shuffle(){
+		
+		//add discard back to the deck
+		deck.addAll(discard);
+		discard.clear();
+		
+		//shuffle the deck
+		for(int i = deck.size(); i>2; i--){
+			
+			int s = ThreadLocalRandom.current().nextInt(i);
+			
+			PlayingCard t = deck.get(s);
+			deck.set(s, deck.get(i));
+			deck.set(i, t);
+		}
+	}
+
 	public int getCardsInDeck(){
 		return deck.size();
 	}
